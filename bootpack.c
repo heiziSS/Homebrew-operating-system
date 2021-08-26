d74ece19c23ff469c4a7f3e729ca94ec12d4ad36
heiziSS <18720931284@163.com>
2021-08-27 00:39:59 +0800

    8.1:鼠标解读（1）

--- bootpack.c
+++ bootpack.c
@@ -10,6 +10,7 @@ void HariMain(void)
     BOOTINFO *binfo = (BOOTINFO *) ADR_BOOTINFO;
     char s[40], mcursor[256], keybuf[32], mousebuf[128];
     int mx, my, i, j;
+    unsigned char mouse_dbuf[3], mouse_phase;
     
     init_gdtidt();
     init_pic();
@@ -32,6 +33,7 @@ void HariMain(void)
     putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
     enable_mouse();
+    mouse_phase = 0;    // 进入到等待鼠标的0xfa状态
 
     for (;;) {
         io_cli();
@@ -47,11 +49,23 @@ void HariMain(void)
             } else {
                 i = fifo8_get(&mousefifo);
                 io_sti();
-                sprintf(s, "%02X", i);
-                boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-                putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                if (mouse_phase == 0) { // 等待鼠标的0xfa状态
+                    if (i == 0xfa)  mouse_phase = 1;
+                } else if (mouse_phase == 1) {  //等待鼠标的第一字节
+                    mouse_dbuf[0] = i;
+                    mouse_phase = 2;
+                } else if (mouse_phase == 2) {  // 等待鼠标的第二字节
+                    mouse_dbuf[1] = i;
+                    mouse_phase = 3;
+                } else if (mouse_phase == 3) {  // 等待鼠标的第三字节
+                    mouse_dbuf[2] = i;
+                    mouse_phase = 1;
+                    /* 鼠标的3个字节集齐，显示出来 */
+                    sprintf(s, "%02X %02X %02X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32+8*8-1, 31);
+                    putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                }
             }
-
         }
     }
 }
