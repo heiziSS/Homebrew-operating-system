02cc83299e3d2c9741f395c23f8d11b352316204
heiziSS <18720931284@163.com>
2021-11-24 23:51:57 +0800

    17.3:切换输入窗口

--- bootpack.c
+++ bootpack.c
@@ -1,6 +1,7 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
 void putfonts8_asc_sht(SHEET *sht, int x, int y, int color, int backColor, char *str, int strLen);
 void make_textbox8(SHEET *sht, int x0, int y0, int sx, int sy, int c);
@@ -30,6 +31,7 @@ void HariMain(void)
 	};
     TASK *task_a, *task_cons;
     TIMER *timer;
+    int key_to = 0;
     
     init_gdtidt();
     init_pic();
@@ -116,7 +118,7 @@ void HariMain(void)
         } else {
             i = fifo_get(&fifo);
             io_sti();
-            if (256 <= i && i < 512) {
+            if (256 <= i && i < 512) { //键盘数据
                 sprintf(s, "%02X", i - 256);
                 putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
                 if (i < 0x54 + 256) {
@@ -130,6 +132,19 @@ void HariMain(void)
                 if (i == 0xe + 256 && cursor_x > 8) { //退格键
                     putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, " ", 1);
                     cursor_x -= 8; // 用空格键把光标消去后，后移1次光标
+                }
+                if (i == 256 + 0x0f) { //tab键
+                    if (key_to == 0) {
+                        key_to = 1;
+                        make_wtitle8(buf_win, sht_win->bxsize, "task_a", 0);
+                        make_wtitle8(buf_cons, sht_cons->bxsize, "console", 1);
+                    } else {
+                        key_to = 0;
+                        make_wtitle8(buf_win, sht_win->bxsize, "task_a", 1);
+                        make_wtitle8(buf_cons, sht_cons->bxsize, "console", 0);
+                    }
+                    sheet_refresh(sht_win, 0, 0, sht_win->bxsize, 21);
+                    sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
                 }
                 // 光标再现
                 boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
@@ -188,6 +203,21 @@ void HariMain(void)
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act)
 {
+    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, xsize - 1, 0);
+    boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, xsize - 2, 1);
+    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, 0, ysize - 1);
+    boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, 1, ysize - 2);
+    boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
+    boxfill8(buf, xsize, COL8_000000, xsize - 1, 0, xsize - 1, ysize - 1);
+    boxfill8(buf, xsize, COL8_C6C6C6, 2, 2, xsize - 3, ysize - 3);
+    boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
+    boxfill8(buf, xsize, COL8_000000, 0, ysize - 1, xsize - 1, ysize - 1);
+    make_wtitle8(buf, xsize, title, act);
+    return;
+}
+
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act)
+{
 	static char closebtn[14][16] = {
 		"OOOOOOOOOOOOOOO@",
 		"OQQQQQQQQQQQQQ$@",
@@ -213,16 +243,7 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char ac
         tc = COL8_C6C6C6;
         tbc = COL8_848484;
     }
-    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, xsize - 1, 0);
-    boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, xsize - 2, 1);
-    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, 0, ysize - 1);
-    boxfill8(buf, xsize, COL8_FFFFFF, 1, 1, 1, ysize - 2);
-    boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
-    boxfill8(buf, xsize, COL8_000000, xsize - 1, 0, xsize - 1, ysize - 1);
-    boxfill8(buf, xsize, COL8_C6C6C6, 2, 2, xsize - 3, ysize - 3);
     boxfill8(buf, xsize, tbc, 3, 3, xsize - 4, 20);
-    boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
-    boxfill8(buf, xsize, COL8_000000, 0, ysize - 1, xsize - 1, ysize - 1);
     putfonts8_asc(buf, xsize, 24, 4, tc, title);
     for (y = 0; y < 14; y++) {
         for (x = 0; x < 16; x++) {
@@ -239,7 +260,6 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char ac
             buf[(5 + y) * xsize + (xsize - 21 + x)] = c;
         }
     }
-    return;
 }
 
 void putfonts8_asc_sht(SHEET *sht, int x, int y, int color, int backColor, char *str, int strLen)
